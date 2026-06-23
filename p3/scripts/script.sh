0487a743c7972bc4c76dd16753291e2f57c13304
Aron BERGMAN <abergman@student.42.fr>
2026-06-23 13:25:54 +0200

    feat(p3): installed argocd

--- p3/scripts/script.sh
+++ p3/scripts/script.sh
@@ -3,12 +3,18 @@
 # ssh-keygen -t ed25519 -C "abergman@student.42.fr"
 # cat ~/.ssh/id_ed25519.pub
 #
+G='\e[32m'
+R='\e[31m'
+Y='\e[33m'
+R='\e[0m'
+#
 # install curl
 sudo apt install -y curl vim
 
-echo "# install docker"
+echo -e "${G}# install docker${R}"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+rm -f get-docker.sh
 
 echo "# install kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -28,3 +34,18 @@ sudo k3d cluster create k3s-default \
   -p 8443:443@loadbalancer
 
 sleep 6
+
+sudo k3d kubeconfig merge k3s-default --kubeconfig-switch-context
+# kubectl get nodes
+
+sleep 6
+
+echo "# create the namespace argocd and dev"
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+
+sleep 6
+
+echo "${G}install ArgoCD${R}"
+# this is config by default
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
