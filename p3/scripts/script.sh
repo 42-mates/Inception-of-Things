af2d5908d02211a56fc9c7a6b57b95ebbc9515e0
Aron BERGMAN <abergman@student.42.fr>
2026-06-23 13:08:45 +0200

    chore(p3): added echo for weite status of installation

--- p3/scripts/script.sh
+++ p3/scripts/script.sh
@@ -4,22 +4,21 @@
 # cat ~/.ssh/id_ed25519.pub
 #
 # install curl
-sudo apt install -y curl
+sudo apt install -y curl vim
 
-# install docker
+echo "# install docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-# install kubectl
+echo "# install kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f kubectl
 
-# install k3d
+echo "# install k3d"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# create the cluster
-#
+echo "# create the cluster"
 sudo k3d cluster delete k3s-default
 sudo k3d cluster create k3s-default \
   --servers 1 \
@@ -27,3 +26,5 @@ sudo k3d cluster create k3s-default \
   --api-port 6443 \
   -p 8088:80@loadbalancer \
   -p 8443:443@loadbalancer
+
+sleep 6
